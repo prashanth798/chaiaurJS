1de85230ec8e07e9b748d42c738cd828c97d3ca8
Prashanth KK <prashanthkk798@gmail.com>
2023-12-17 14:09:29 +0530

    Math and Number

---
+++ 01basics/Number.js
@@ -0,0 +1,21 @@
+const num = 56000.78946;
+
+const num1 = new Number(300)
+
+// console.table([num, num1]);
+
+// console.log(123.8789.toFixed(3)); //500.000    123.870
+
+// console.table([num.toFixed(3), num.toPrecision(4), num.toLocaleString("en-IN")]);
+
+console.table([Math.min(3,4,5),
+    Math.max(8,88,894,6,4),
+    Math.floor(4.9),
+    Math.ceil(3.1),
+    Math.random()
+]);
+
+max = 20;
+min = 10;
+
+console.log(Math.floor((Math.random())* (max - min +1)+min));
---
+++ 01basics/String.js
@@ -0,0 +1,18 @@
+const name ="programming";
+const stringName= new String("Mann")
+
+
+console.table([name, stringName]);
+console.log(typeof stringName)
+//Check for String methods through mdn 
+/*
+trim()
+length
+toUpperCase()
+toLowerCase()
+split() 0,4,    -8,3
+replace()
+indexOf()
+startsWith()
+endsWith()
+*/
